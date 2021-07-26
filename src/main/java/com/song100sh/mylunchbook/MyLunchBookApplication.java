f47c17cbb3e046b04cb28824753ab11cdc25933f
song100sh <song100sh@gmail.com>
2021-07-26 16:51:02 +0900

    spring add react

---
+++ src/main/java/com/song100sh/mylunchbook/MyLunchBookApplication.java
@@ -0,0 +1,21 @@
+package com.song100sh.mylunchbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MyLunchBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLunchBookApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World Spring";
+    }
+
+}
---
+++ src/test/java/com/song100sh/mylunchbook/MyLunchBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song100sh.mylunchbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLunchBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
